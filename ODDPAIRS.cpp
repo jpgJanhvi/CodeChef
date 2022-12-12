a008897c55d08519340bcb35a1eb1f4b3a338129
janhvi pandey <94743766+jpgJanhvi@users.noreply.github.com>
2022-12-12 16:56:57 +0530

    Add files via upload

---
+++ ODDPAIRS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long int n,p,ans;
+	    cin>>n;
+	    p=n*n;
+	    ans=p/2;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
